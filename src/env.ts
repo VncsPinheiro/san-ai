5c3f324d268a591df94ee4a13f345fb3c8086048
Vinicius <viniciuspinheiro932@gmail.com>
2025-12-03 07:37:31 -0300

    fix: model add in .env

--- src/env.ts
+++ src/env.ts
@@ -5,7 +5,8 @@ config()
 const envSchema = z.object({
   PORT: z.coerce.number().default(3000),
   DATABASE_URL: z.string().startsWith('postgresql://'),
-  API_KEY: z.string()
+  API_KEY: z.string(),
+  SAN_MODEL: z.enum(['gemini-2.5-flash', 'gemini-2.5-flash-lite', 'gemini-2.5-flash-pro', 'gemini-3.0-pro-preview']).default('gemini-2.5-flash')
 })
 
 export const env = envSchema.parse(process.env)
